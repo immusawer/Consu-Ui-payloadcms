b5b0fe4fcf28718d458b0e58cce597f704bcd5ac
Musawer <musawerdelair2020@gmail.com>
2025-04-23 18:19:17 +0430

    changes

--- src/components/process/ProcessStyle1.jsx
+++ src/components/process/ProcessStyle1.jsx
@@ -49,6 +49,20 @@ const ProcessStyle1 = ({ sectionClass }) => {
   return (
     <div className={`process-style-one-area text-center default-padding ${sectionClass || ''}`}>
       {/* ... rest of your JSX ... */}
+      <div className="large-shape">
+                    <img src={shape11} alt="Shape" />
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-8 offset-lg-2">
+                            <div className="site-heading text-center">
+                                <h4 className="sub-heading">Our Process</h4>
+                                <h2 className="title">Get to Know Our <br /> work process</h2>
+                                <div className="devider"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
       <div className="container">
         <div className="row">
           {processData.map((process) => (
